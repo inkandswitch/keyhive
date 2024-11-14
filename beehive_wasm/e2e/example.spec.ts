49f1638b3037d12d772e9eeda4a9290a80092702
Brooklyn Zelenka <hello@brooklynzelenka.com>
2024-11-13 23:38:45 -0800

    Break out tests

--- beehive_wasm/e2e/example.spec.ts
+++ beehive_wasm/e2e/example.spec.ts
@@ -9,37 +9,50 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("SigningKey", async () => {
-  test('constructor', async ({ page }) => {
-    const out = await page.evaluate(() => {
+  test.describe('constructor', async () => {
+    const scenario = () => {
       const { SigningKey } = window.beehive
       const key = SigningKey.generate()
       return { key }
-    })
+    }
 
-    expect(out.key).toBeDefined()
+    test('initializes successfully', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.key).toBeDefined()
+    })
   })
 
-  test('verifyingKey', async ({ page }) => {
-    const out = await page.evaluate((input) => {
+  test.describe('verifyingKey', async () => {
+    const scenario = (input) => {
       const { SigningKey } = window.beehive
       const key = SigningKey.generate()
       return { input, key, vKey: key.verifyingKey }
-    }, { toSign })
+    }
 
-    expect(out.vKey).toBeDefined()
+    test('has a verifying key', async ({ page }) => {
+      const out = await page.evaluate(scenario, { toSign })
+      expect(out.vKey).toBeDefined()
+    })
   })
 
-  test('trySign', async ({ page }) => {
-    const out = await page.evaluate((input) => {
+  test.describe('trySign', async () => {
+    const scenario = (input) => {
       const { SigningKey } = window.beehive
       const key = SigningKey.generate()
       const signed = key.trySign(new Uint8Array(input.toSign))
       const { payload, verifyingKey, signature } = signed
       return { input, payload, verifyingKey, signature, key }
-    }, { toSign })
+    }
 
-    expect(out.signature).toBeDefined()
-    expect(Object.values(out.payload)).toStrictEqual(toSign)
+    test('has a signature', async ({ page }) => {
+      const out = await page.evaluate(scenario, { toSign })
+      expect(out.signature).toBeDefined()
+    })
+
+    test('embeds the payload unchanged', async ({ page }) => {
+      const out = await page.evaluate(scenario, { toSign })
+      expect(Object.values(out.payload)).toStrictEqual(toSign)
+    })
   })
 })
 
@@ -62,8 +75,7 @@ test.describe("Signed", async () => {
       const { SigningKey } = window.beehive
       const key = SigningKey.generate()
       const signed = key.trySign(new Uint8Array(input.toSign))
-      const { payload, verifyingKey, signature } = signed
-      return { input, payload, verifyingKey, signature, key }
+      return { payload: signed.payload }
     }, { toSign })
 
     expect(Object.values(out.payload)).toStrictEqual(toSign)
@@ -74,12 +86,10 @@ test.describe("Signed", async () => {
       const { SigningKey } = window.beehive
       const key = SigningKey.generate()
       const signed = key.trySign(new Uint8Array(input.toSign))
-      const { payload, signature } = signed
-      return { input, payload, signature }
+      return { signature: signed.signature }
     }, { toSign })
 
     expect(out.signature).toBeDefined()
-    expect(Object.values(out.payload)).toStrictEqual(toSign)
   })
 })
 
@@ -105,23 +115,28 @@ test.describe("Beehive", async () => {
     expect(out.id).toStrictEqual(out.vKey)
   })
 
-  test('idString', async ({ page }) => {
-    const out = await page.evaluate(() => {
+  test.describe('idString', async () => {
+    const scenario = () => {
       const { Beehive, SigningKey } = window.beehive
       const key = SigningKey.generate()
       const vKey = key.verifyingKey
       const beehive = new Beehive(key)
       return { idString: beehive.idString, vKey }
+    }
+
+    test('is >= 66 charecters', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.idString.length).toBeLessThanOrEqual(66)
     })
 
-    expect(out.idString).toBeDefined()
-    expect(out.idString.length).toBeLessThanOrEqual(66)
-    expect(out.idString.slice(0, 2)).toStrictEqual('0x')
-    expect(out.idString).toMatch(/0x[0-9a-fA-F]+/) // Hex
+    test('is a hex string starting with 0x', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.idString).toMatch(/0x[0-9a-fA-F]+/)
+    })
   })
 
-  test('generateGroup', async ({ page }) => {
-    const out = await page.evaluate(() => {
+  test.describe('generateGroup', async () => {
+    const scenario = () => {
       const { Beehive, SigningKey } = window.beehive
       const beehive = new Beehive(SigningKey.generate())
 
@@ -129,11 +144,26 @@ test.describe("Beehive", async () => {
       const { groupId, members } = group
       const canStr = members[0].can.toString()
       return { group, groupId, members, canStr }
+    }
+
+    test('makes a new group', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.group).toBeDefined()
     })
 
-    expect(out.group).toBeDefined()
-    expect(out.groupId).toBeDefined()
-    expect(out.members).toHaveLength(1)
-    expect(out.canStr).toStrictEqual('Admin')
+    test('the associated group has an groupId (is an actual group)', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.groupId).toBeDefined()
+    })
+
+    test('group has exacty one member', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.members).toHaveLength(1)
+    })
+
+    test('the sole group member is an admin', async ({ page }) => {
+      const out = await page.evaluate(scenario)
+      expect(out.canStr).toStrictEqual('Admin')
+    })
   })
 })
