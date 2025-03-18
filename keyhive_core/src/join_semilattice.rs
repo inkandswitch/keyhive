ee3e3f3b642edb777d50e8da53ed2ebd9fea8614
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-03-18 00:31:57 -0700

    Async variant

--- keyhive_core/src/join_semilattice.rs
+++ keyhive_core/src/join_semilattice.rs
@@ -11,7 +11,7 @@ use std::{
     rc::Rc,
 };
 
-pub trait Forkable: Sized {
+pub(crate) trait Forkable: Sized {
     fn fork(&self) -> Self;
 }
 
@@ -21,21 +21,10 @@ impl<T: Clone> Forkable for T {
     }
 }
 
-pub trait JoinSemilattice: Forkable {
+pub(crate) trait JoinSemilattice: Forkable {
     // Converge ; note: this needs to run _rebuold
     // // FIXME unsafe megre so that we can use it in transact and not fail?
     fn merge(&mut self, mut other: Self);
-
-    // FIXME move to associated type and make public
-    fn transact<F: FnMut(&mut Self) -> Result<(), Error>, Error>(
-        &mut self,
-        mut fun: F,
-    ) -> Result<(), Error> {
-        let mut forked = self.fork();
-        fun(&mut forked)?;
-        self.merge(forked);
-        Ok(())
-    }
 }
 
 impl<T: Hash + Eq + Clone> JoinSemilattice for HashSet<T> {
@@ -61,4 +50,26 @@ impl<T> JoinSemilattice for Rc<RefCell<T>> {
 
 // FIXME also provdide a way to compare heads
 
-// FIXME RC Refcell
+pub fn transact<T: JoinSemilattice, F: FnMut(&mut T) -> Result<(), Error>, Error>(
+    semilattice: &mut T,
+    mut fun: F,
+) -> Result<(), Error> {
+    let mut forked = semilattice.fork();
+    fun(&mut forked)?;
+    semilattice.merge(forked);
+    Ok(())
+}
+
+pub async fn transact_async<
+    T: JoinSemilattice,
+    F: AsyncFnMut(&mut T) -> Result<(), Error>,
+    Error,
+>(
+    semilattice: &mut T,
+    mut fun: F,
+) -> Result<(), Error> {
+    let mut forked = semilattice.fork();
+    fun(&mut forked).await?;
+    semilattice.merge(forked);
+    Ok(())
+}
