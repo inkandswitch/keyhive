47fc29a2fbaaccd38d23d899d28def457aa5558c
Alex Good <alex@memoryandthought.me>
2025-09-19 15:39:02 +0100

    keyhive-js: Remove async keyhive constructor

--- keyhive_wasm/e2e/document.spec.ts
+++ keyhive_wasm/e2e/document.spec.ts
@@ -1,28 +1,32 @@
-import { test, expect } from '@playwright/test';
-import { URL } from './config';
+import { test, expect } from "@playwright/test";
+import { URL } from "./config";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(URL)
-  await page.waitForFunction(() => !!window.keyhive)
+  await page.goto(URL);
+  await page.waitForFunction(() => !!window.keyhive);
 });
 
 test.describe("Document", async () => {
-  test('constructor', async ({ page }) => {
+  test("constructor", async ({ page }) => {
     const out = await page.evaluate(async () => {
-      const { Keyhive, Signer, ChangeRef, CiphertextStore } = window.keyhive
+      const { Keyhive, Signer, ChangeRef, CiphertextStore } = window.keyhive;
 
-      const store = CiphertextStore.newInMemory()
-      const bh = await new Keyhive(await new Signer(), store, console.log)
+      const store = CiphertextStore.newInMemory();
+      const bh = await Keyhive.init(
+        await Signer.generate(),
+        store,
+        console.log,
+      );
       const changeRef = new ChangeRef(new Uint8Array([1, 2, 3]));
 
-      const g = await bh.generateGroup([])
-      const doc = await bh.generateDocument([g.toPeer()], changeRef, [])
-      const docId = doc.id
+      const g = await bh.generateGroup([]);
+      const doc = await bh.generateDocument([g.toPeer()], changeRef, []);
+      const docId = doc.id;
 
-      return { doc, docId }
-    })
+      return { doc, docId };
+    });
 
-    expect(out.doc).toBeDefined()
-    expect(out.docId).toBeDefined()
-  })
-})
+    expect(out.doc).toBeDefined();
+    expect(out.docId).toBeDefined();
+  });
+});
