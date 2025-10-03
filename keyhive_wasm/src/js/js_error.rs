738fad8998cb18a9f60a4cb973adc6eedcb1bf40
John Mumm <jtfmumm@gmail.com>
2025-10-03 11:54:15 +0100

    Switch to JsErrors

---
+++ keyhive_wasm/src/js/js_error.rs
@@ -0,0 +1,37 @@
+use keyhive_core::{
+    keyhive::EncryptContentError,
+    principal::{
+        document::{AddMemberError, DecryptError, GenerateDocError},
+        group::RevokeMemberError,
+        individual::ReceivePrekeyOpError,
+    },
+    crypto::signed::SigningError,
+};
+use thiserror::Error;
+use wasm_bindgen::prelude::*;
+
+#[derive(Debug, Error)]
+pub enum JsError {
+    #[error(transparent)]
+    GenerateDoc(#[from] GenerateDocError),
+    #[error(transparent)]
+    AddMember(#[from] AddMemberError),
+    #[error(transparent)]
+    RevokeMember(#[from] RevokeMemberError),
+    #[error(transparent)]
+    EncryptContent(#[from] EncryptContentError),
+    #[error(transparent)]
+    Decrypt(#[from] DecryptError),
+    #[error(transparent)]
+    ReceivePrekeyOp(#[from] ReceivePrekeyOpError),
+    #[error(transparent)]
+    Signing(#[from] SigningError),
+    #[error("{0}")]
+    Other(String),
+}
+
+impl From<JsError> for JsValue {
+    fn from(e: JsError) -> Self {
+        JsValue::from_str(&e.to_string())
+    }
+}
