0a15660945e351d33515af7173d5571edfc52912
John Mumm <jtfmumm@gmail.com>
2025-10-29 16:30:41 +0000

    Implement Display

--- keyhive_wasm/src/js/stats.rs
+++ keyhive_wasm/src/js/stats.rs
@@ -1,10 +1,21 @@
 use keyhive_core::stats::Stats;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen(js_name = Stats)]
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct JsStats(pub(crate) Stats);
+
+impl fmt::Display for JsStats {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Stats {{ individuals: {}, groups: {}, docs: {}, delegations: {}, revocations: {} }}",
+            self.0.individuals, self.0.groups, self.0.docs, self.0.delegations, self.0.revocations
+        )
+    }
+}
 
 #[wasm_bindgen(js_class = Stats)]
 impl JsStats {
@@ -31,13 +42,5 @@ impl JsStats {
     #[wasm_bindgen(getter)]
     pub fn revocations(&self) -> u64 {
         self.0.revocations
-    }
-
-    #[wasm_bindgen(js_name = toString)]
-    pub fn to_string(&self) -> String {
-        format!(
-            "Stats {{ individuals: {}, groups: {}, docs: {}, delegations: {}, revocations: {} }}",
-            self.0.individuals, self.0.groups, self.0.docs, self.0.delegations, self.0.revocations
-        )
     }
 }
