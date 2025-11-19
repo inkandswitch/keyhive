93ffe26ab347a8ac6a3fc4bf4daaae59d84f91d6
John Mumm <jtfmumm@gmail.com>
2025-11-19 14:42:19 +0000

    Add more fields to JsStats

--- keyhive_wasm/src/js/stats.rs
+++ keyhive_wasm/src/js/stats.rs
@@ -11,8 +11,23 @@ impl fmt::Display for JsStats {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "Stats {{ individuals: {}, groups: {}, docs: {}, delegations: {}, revocations: {}, active_prekey_count: {} }}",
-            self.0.individuals, self.0.groups, self.0.docs, self.0.delegations, self.0.revocations, self.0.active_prekey_count
+            "Stats {{ individuals: {}, groups: {}, docs: {}, delegations: {}, revocations: {}, active_prekey_count: {}, pending_prekeys_expanded: {}, pending_prekeys_expanded_by_active: {}, pending_prekey_rotated: {}, pending_prekey_rotated_by_active: {}, pending_cgka_operation: {}, pending_cgka_operation_by_active: {}, pending_delegated: {}, pending_delegated_by_active: {}, pending_revoked: {}, pending_revoked_by_active: {} }}",
+            self.0.individuals,
+            self.0.groups,
+            self.0.docs,
+            self.0.delegations,
+            self.0.revocations,
+            self.0.active_prekey_count,
+            self.0.pending_prekeys_expanded,
+            self.0.pending_prekeys_expanded_by_active,
+            self.0.pending_prekey_rotated,
+            self.0.pending_prekey_rotated_by_active,
+            self.0.pending_cgka_operation,
+            self.0.pending_cgka_operation_by_active,
+            self.0.pending_delegated,
+            self.0.pending_delegated_by_active,
+            self.0.pending_revoked,
+            self.0.pending_revoked_by_active
         )
     }
 }
@@ -47,5 +62,55 @@ impl JsStats {
     #[wasm_bindgen(getter, js_name = activePrekeyCount)]
     pub fn active_prekey_count(&self) -> u64 {
         self.0.active_prekey_count
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingPrekeysExpanded)]
+    pub fn pending_prekeys_expanded(&self) -> u64 {
+        self.0.pending_prekeys_expanded
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingPrekeysExpandedByActive)]
+    pub fn pending_prekeys_expanded_by_active(&self) -> u64 {
+        self.0.pending_prekeys_expanded_by_active
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingPrekeyRotated)]
+    pub fn pending_prekey_rotated(&self) -> u64 {
+        self.0.pending_prekey_rotated
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingPrekeyRotatedByActive)]
+    pub fn pending_prekey_rotated_by_active(&self) -> u64 {
+        self.0.pending_prekey_rotated_by_active
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingCgkaOperation)]
+    pub fn pending_cgka_operation(&self) -> u64 {
+        self.0.pending_cgka_operation
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingCgkaOperationByActive)]
+    pub fn pending_cgka_operation_by_active(&self) -> u64 {
+        self.0.pending_cgka_operation_by_active
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingDelegated)]
+    pub fn pending_delegated(&self) -> u64 {
+        self.0.pending_delegated
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingDelegatedByActive)]
+    pub fn pending_delegated_by_active(&self) -> u64 {
+        self.0.pending_delegated_by_active
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingRevoked)]
+    pub fn pending_revoked(&self) -> u64 {
+        self.0.pending_revoked
+    }
+
+    #[wasm_bindgen(getter, js_name = pendingRevokedByActive)]
+    pub fn pending_revoked_by_active(&self) -> u64 {
+        self.0.pending_revoked_by_active
     }
 }
