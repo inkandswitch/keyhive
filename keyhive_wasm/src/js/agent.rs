f0ab084633bbddc4561155fce42e8a39c70ba2f7
John Mumm <jtfmumm@gmail.com>
2025-12-05 10:05:58 +0000

    Add keyOps method to JsAgent

--- keyhive_wasm/src/js/agent.rs
+++ keyhive_wasm/src/js/agent.rs
@@ -1,9 +1,10 @@
 use super::{
-    change_id::JsChangeId, event_handler::JsEventHandler, identifier::JsIdentifier,
+    change_id::JsChangeId, event::JsEvent, event_handler::JsEventHandler, identifier::JsIdentifier,
     signer::JsSigner,
 };
 use derive_more::{Deref, Display, From, Into};
-use keyhive_core::principal::agent::Agent;
+use dupe::Dupe;
+use keyhive_core::{crypto::digest::Digest, event::Event, principal::agent::Agent};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(js_name = Agent)]
@@ -42,5 +43,20 @@ impl JsAgent {
     #[wasm_bindgen(getter)]
     pub fn id(&self) -> JsIdentifier {
         JsIdentifier(self.0.id())
+    }
+
+    /// Returns prekey operations for this agent as a Map of hash -> Event
+    #[wasm_bindgen(js_name = keyOps)]
+    pub async fn key_ops(&self) -> js_sys::Map {
+        let key_ops = self.0.key_ops().await;
+        let map = js_sys::Map::new();
+        for key_op in key_ops {
+            let event = Event::from(key_op.as_ref().dupe());
+            let digest = Digest::hash(&event);
+            let hash = js_sys::Uint8Array::from(digest.as_slice());
+            let js_event = JsEvent::from(event);
+            map.set(&hash.into(), &JsValue::from(js_event));
+        }
+        map
     }
 }
