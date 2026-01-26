ec5d6dbc6f5ed997c3cb3eb2e074e0bca8fb58fa
John Mumm <jtfmumm@gmail.com>
2026-01-26 16:58:49 +0000

    Memoize op digests and avoid duplicate ancestors lookups

--- keyhive_wasm/src/js/agent.rs
+++ keyhive_wasm/src/js/agent.rs
@@ -51,6 +51,7 @@ impl JsAgent {
 
     /// Returns prekey operations for this agent as a Map of hash -> serialized bytes for [`StaticEvent`]
     #[wasm_bindgen(js_name = keyOps)]
+    #[allow(clippy::mutable_key_type)]
     pub async fn key_ops(&self) -> Result<js_sys::Map, JsSerializationError> {
         let key_ops = self.0.key_ops().await;
         let map = js_sys::Map::new();
