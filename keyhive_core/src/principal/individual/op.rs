0960d321fada5becd59a346ffc19fff8f3b55765
John Mumm <jtfmumm@gmail.com>
2026-03-05 12:27:50 +0000

    Implement reachable_prekey_ops_for_all

--- keyhive_core/src/principal/individual/op.rs
+++ keyhive_core/src/principal/individual/op.rs
@@ -4,18 +4,18 @@ pub mod add_key;
 pub mod rotate_key;
 
 use self::{add_key::AddKeyOp, rotate_key::RotateKeyOp};
-use crate::crypto::{
-    share_key::ShareKey,
-    signed::{Signed, VerificationError},
-    verifiable::Verifiable,
+use crate::{
+    crypto::{
+        share_key::ShareKey,
+        signed::{Signed, VerificationError},
+        verifiable::Verifiable,
+    },
+    util::content_addressed_map::CaMap,
 };
 use derive_more::{From, TryInto};
 use dupe::Dupe;
 use serde::{Deserialize, Serialize};
-use std::{
-    collections::{HashMap, HashSet},
-    sync::Arc,
-};
+use std::{collections::HashMap, sync::Arc};
 
 /// Operations for updating prekeys.
 ///
@@ -33,22 +33,20 @@ pub enum KeyOp {
 }
 
 impl KeyOp {
-    pub fn topsort(key_ops: HashSet<KeyOp>) -> Vec<KeyOp> {
-        let mut heads: Vec<KeyOp> = vec![];
-        let mut rotate_key_ops: HashMap<ShareKey, HashSet<KeyOp>> = HashMap::new();
+    pub fn topsort(key_ops: &CaMap<KeyOp>) -> Vec<Arc<KeyOp>> {
+        let mut heads: Vec<Arc<KeyOp>> = vec![];
+        let mut rotate_key_ops: HashMap<ShareKey, Vec<Arc<KeyOp>>> = HashMap::new();
 
-        for key_op in key_ops.iter() {
-            match key_op {
+        for key_op in key_ops.values() {
+            match key_op.as_ref() {
                 KeyOp::Add(_add) => {
                     heads.push(key_op.dupe());
                 }
                 KeyOp::Rotate(rot) => {
                     rotate_key_ops
                         .entry(rot.payload.old)
-                        .and_modify(|set| {
-                            set.insert(key_op.dupe());
-                        })
-                        .or_insert(HashSet::from_iter([key_op.dupe()]));
+                        .or_default()
+                        .push(key_op.dupe());
                 }
             }
         }
